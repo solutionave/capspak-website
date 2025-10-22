daa1df4c888d5aa480bb20d61225967a2309da63
hammadbinnoman52-art <hammadbinnoman52@gmail.com>
2025-10-22 15:34:21 +0500

    data uploaded in news update

--- app/internship/page.tsx
+++ app/internship/page.tsx
@@ -2,10 +2,17 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      Internship
+      <div className="text-center">
+      <img
+        src="/Assets/Opportunities/first-intern-cohort.png"
+        width={550}
+        height={550}
+        className="mx-auto"
+      />
+      <h1 className="text-2xl font-bold mt-4">
+        CAPES First Intern Cohort - Summer 2025
+      </h1>
     </div>
   )
 }
-
 export default page
