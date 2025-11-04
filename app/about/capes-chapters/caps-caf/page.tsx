f9d12bef0cedbc5f85e8d76e4a51b4af688f7f25
hammadbinnoman52-art <hammadbinnoman52@gmail.com>
2025-11-04 10:17:10 +0500

    ummmm brousher uploadded in CAPS-caf

--- app/about/capes-chapters/caps-caf/page.tsx
+++ app/about/capes-chapters/caps-caf/page.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import Image from "next/image";
 
 const page = () => {
   return (
-    <div>
-      CAPES-CAF
-    </div>
+      <div className="bg-white shadow-lg rounded-2xl p-6 w-[40rem] hover:shadow-xl transition-shadow duration-300">
+        <Image
+        src="/Assets/HomePageSlider/logoo.jpeg"
+        alt="CAPES Map"
+        width={600}     
+        height={300} 
+        className='border border-radius[8px]'
+     />
+      </div>
   )
 }
 
