c520ee55211291461b05da6f22cec1c4f066498b
Taleeha Tahoor <taleeha789@gmail.com>
2025-11-04 10:57:57 +0500

    change image name and position

--- app/about/capes-chapters/caps-caf/page.tsx
+++ app/about/capes-chapters/caps-caf/page.tsx
@@ -5,9 +5,9 @@ const page = () => {
   return (
       <div className="bg-white shadow-lg rounded-2xl p-6 w-[40rem] hover:shadow-xl transition-shadow duration-300">
         <Image
-        src="/Assets/HomePageSlider/logoo.jpeg"
+        src="/Assets/Opportunities/Climate action forum.jpeg"
         alt="CAPES Map"
-        width={600}     
+        width={600}      
         height={300} 
         className='border border-radius[8px]'
      />
