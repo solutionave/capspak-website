7641d921b57d736d865f4d37bb72f1d4d17350ce
Taleeha Tahoor <taleeha789@gmail.com>
2026-07-29 12:03:23 +0500

    production size issue

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,20 @@ const nextConfig: NextConfig = {
       "./public/docs/**",
       "./public/logos/**",
     ],
+    "/weeklynewsmonitor/page": [
+      "./public/Assets/NewsImages/**",
+      "./public/Assets/Events/**",
+      "./public/Assets/HomePageSlider/**",
+      "./public/Assets/Newsletter/**",
+      "./public/Assets/PolicyBrief/**",
+      "./public/Assets/Team/**",
+      "./public/Assets/Opportunities/**",
+      "./public/Assets/capsthinkpoint/**",
+      "./public/Assets/About_bgimg.png",
+      "./public/Assets/President_img.JPG",
+      "./public/docs/**",
+      "./public/logos/**",
+    ],
   },
 };
 
